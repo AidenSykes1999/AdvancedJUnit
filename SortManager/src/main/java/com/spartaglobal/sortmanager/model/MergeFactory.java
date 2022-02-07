be98ed6e7ad3a43f6cb89a37aa29d1950972c7d9
AidenSykes1999 <93606534+AidenSykes1999@users.noreply.github.com>
2022-02-07 14:35:36 +0000

    Delete SortManager directory

--- SortManager/src/main/java/com/spartaglobal/sortmanager/model/MergeFactory.java
+++
@@ -1,9 +0,0 @@
-package com.spartaglobal.sortmanager.model;
-
-public class MergeFactory extends SorterFactory{
-    @Override
-    public Sorter getInstance() {
-        System.out.println("Merging an Array for you!");
-        return new MergeSorter();
-    }
-}
--- SortManager/src/main/java/com/spartaglobal/sortmanager/model/Sorter.java
+++
@@ -1,8 +0,0 @@
-package com.spartaglobal.sortmanager.model;
-
-public interface Sorter {
-
-    String sort();
-
-
-}
